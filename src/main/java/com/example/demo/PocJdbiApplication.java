a474aec468bc742db7d1b076eddc618fc5124d2d
gabriellopesk <gabriellopesk@gmail.com>
2021-06-15 00:37:14 -0300

    Init project

---
+++ src/main/java/com/example/demo/PocJdbiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJdbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJdbiApplication.class, args);
+	}
+
+}
